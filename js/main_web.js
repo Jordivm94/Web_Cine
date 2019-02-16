6832e7a1ffcc0c47f000b2d776a7ee1811a708e3
Jordivm94 <47102381+Jordivm94@users.noreply.github.com>
2019-02-16 15:52:33 +0100

    Add files via upload

---
+++ js/main_web.js
@@ -0,0 +1,9 @@
+ window.onload = function() {
+
+  	document.getElementById("img1").onclick = direccion;
+ 
+ 	function direccion(){
+ 		var pagina= "html/fitxa_peli.html"
+ 		document.location.href = pagina;
+ 	}
+ }
