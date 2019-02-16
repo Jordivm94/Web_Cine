813209b6e0a5f164743fdfc72af730b08271a14f
Jordivm94 <47102381+Jordivm94@users.noreply.github.com>
2019-02-16 21:59:47 +0100

    Add files via upload

--- js/main_web.js
+++ js/main_web.js
@@ -1,9 +1,55 @@
  window.onload = function() {
 
   	document.getElementById("img1").onclick = direccion;
- 
+ 	document.getElementById("img2").onclick = direccion2;
+ 	document.getElementById("img3").onclick = direccion3;
+ 	document.getElementById("img4").onclick = direccion4;
+ 	document.getElementById("img5").onclick = direccion5;
+ 	document.getElementById("img6").onclick = direccion6;
+ 	document.getElementById("img7").onclick = direccion7;
+ 	document.getElementById("img8").onclick = direccion8;
+ 	document.getElementById("img9").onclick = direccion9;
  	function direccion(){
  		var pagina= "html/fitxa_peli.html"
  		document.location.href = pagina;
  	}
+
+ 	function direccion2(){
+ 		var pagina= "html/fitxa_peli2.html"
+ 		document.location.href = pagina;
+ 	}
+ 	function direccion3(){
+ 		var pagina= "html/fitxa_peli3.html"
+ 		document.location.href = pagina;
+ 	}
+
+	function direccion4(){
+ 		var pagina= "html/fitxa_peli4.html"
+ 		document.location.href = pagina;
+ 	}
+
+	function direccion5(){
+ 		var pagina= "html/fitxa_peli5.html"
+ 		document.location.href = pagina;
+ 	}
+
+	function direccion6(){
+ 		var pagina= "html/fitxa_peli6.html"
+ 		document.location.href = pagina;
+ 	}
+
+	function direccion7(){
+ 		var pagina= "html/fitxa_peli7.html"
+ 		document.location.href = pagina;
+ 	}
+ 	function direccion8(){
+ 		var pagina= "html/fitxa_peli8.html"
+ 		document.location.href = pagina;
+ 	}
+	function direccion9(){
+ 		var pagina= "html/fitxa_peli9.html"
+ 		document.location.href = pagina;
+ 	}
+
+
  }
